ba72c8510ca2e6bdbbfdfab2e1957974c2f40abe
abdalimran <abdalimran@gmail.com>
2016-06-03 13:09:04 +0600

    Updated onDestroy() and duration.

--- app/src/main/java/com/github/abdalimran/splashscreen/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/github/abdalimran/splashscreen/splashscreen/SplashScreenActivity.java
@@ -18,8 +18,11 @@ import java.util.TimerTask;
 
 
 public class SplashScreenActivity extends Activity {
-    final static int SPLASH_TIME_OUT = 3000;
+    final static int SPLASH_TIME_OUT = 4000;
+    private boolean scheduled = false;
+    private Timer timer;
 
+    @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         Window window = getWindow();
@@ -46,7 +49,7 @@ public class SplashScreenActivity extends Activity {
         splashimg.clearAnimation();
         splashimg.startAnimation(anim);
 
-        Timer timer = new Timer();
+        timer = new Timer();
         timer.schedule(new TimerTask(){
             @Override
             public void run() {
@@ -54,5 +57,16 @@ public class SplashScreenActivity extends Activity {
                 startActivity(home_page);
                 finish();
             }}, SPLASH_TIME_OUT);
+        this.scheduled=true;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if (this.scheduled) {
+            this.timer.cancel();
+        }
+        this.timer.purge();
     }
 }
